ae65485497b934c3761d1d61e03bccf1cbc69e4d
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-09 10:47:02 -0400

    Update AppliedNumerology.go

--- AppliedNumerology.go
+++ AppliedNumerology.go
@@ -3,6 +3,6 @@ import "fmt"
 func main() {
     fmt.Println("Applied Numerology")
     fmt.Println("")
-    fmt.Println("x^2 + 4 is metallic numbers, which can be linked each to culture")")
+    fmt.Println("x^2 + 4 is metallic numbers, which can be linked each to culture")
     fmt.Println("Enneagram type can be described as follows: https://sacredartofliving.org/wp-content/uploads/2020/10/Brief-Descriptions-of-the-18-Enneagram-Types-With-Wings.pdf")
 }
