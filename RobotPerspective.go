baa362a21ed9c2b82d43672d02c033694c98a52d
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-09 10:54:19 -0400

    Update and rename r.html to RobotPerspective.go

---
+++ RobotPerspective.go
@@ -0,0 +1,7 @@
+package RobotPerspective
+import "fmt"
+func main() {
+    fmt.Println("RobotPerspective")
+    fmt.Println("")
+    fmt.Println("Robots need degrees of freedom to make step-by-step decisions. As reality is more often made of objects than subjects, it is easy for robots to gain understanding.")
+}
