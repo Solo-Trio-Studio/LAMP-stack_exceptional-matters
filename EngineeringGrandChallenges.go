1f46ca5ff8b5ab01cba14a629f0e2ea179c690f8
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-09 10:52:23 -0400

    Update and rename g.html to EngineeringGrandChallenges.go

---
+++ EngineeringGrandChallenges.go
@@ -0,0 +1,12 @@
+package EngineeringGrandChallenges 
+import "fmt"
+func main() {
+    fmt.Println("Engineering Grand Challenges")
+    fmt.Println("")
+    fmt.Println("Solar Energy: consider solar energy at least a here and now priority.")
+    fmt.Println("Fusion: plan on using the right kind of nuclear energy extensively into the future.")
+    fmt.Println("Carbon Sequestration: advance understanding of climate change.")
+    fmt.Println("Security:" prevent Malware-as-a-Service.")
+    fmt.Println("Health Informatics: increase reliability of wellness tech.")
+    fmt.Println("Medicines: emphasize cures.")
+}
