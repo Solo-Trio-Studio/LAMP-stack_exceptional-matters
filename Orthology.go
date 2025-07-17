7ee3aa360c53bfdd34529f6250ccb2c0f8b471fe
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-17 19:07:21 -0400

    Update Orthology.go

--- Orthology.go
+++ Orthology.go
@@ -1,5 +1,5 @@
 package Orthology
 import "fmt"
 func main() {
-   fmt.PrintLn("C/J/O/S/U are subjective. Remaining (including blankspace) are philosophical. E, F, H, I, L, T are objective.")
+   fmt.Println("C/J/O/S/U are subjective. Remaining (including blankspace) are philosophical. E, F, H, I, L, T are objective.")
 }
